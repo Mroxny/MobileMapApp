baa3e5ef686ecf111383bec4e683416995d0e5ca
Mroxny <50219282+Mroxny@users.noreply.github.com>
2021-10-31 15:12:35 +0100

    Update 31.10.2021
    - pre searching activity
    - osm bonuspack library  added

--- app/src/main/java/com/mroxny/mobilemapapp/SearchActivity.java
+++ app/src/main/java/com/mroxny/mobilemapapp/SearchActivity.java
@@ -1,17 +1,31 @@
 package com.mroxny.mobilemapapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 
+import org.osmdroid.bonuspack.location.GeocoderNominatim;
+
+import java.util.ArrayList;
+
 public class SearchActivity extends AppCompatActivity {
+
+    private GeocoderNominatim geocoder = new GeocoderNominatim("OsmNavigator/2.4");
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchView = (SearchView) findViewById(R.id.sv);
+        searchView.setIconifiedByDefault(false);
+        searchView.requestFocus();
     }
 
     public void goBack(View view){
